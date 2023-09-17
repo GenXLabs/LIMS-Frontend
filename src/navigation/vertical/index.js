9241f16257681c5759d7d76657ecfd8e1a6b0404
Sandupa Abeywardana <74253035+SandupaAbeywardana@users.noreply.github.com>
2023-09-17 14:02:24 +0530

    created file structure

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -19,6 +19,16 @@ const navigation = () => {
       path: '/student/lab-reservation/create-request',
       title: 'Request Reservation',
       icon: 'tabler:calendar-event'
+    },
+    {
+      path: '/student/lab-reservation/view-requests',
+      title: 'View Reservations',
+      icon: 'ion:calendar-outline'
+    },
+    {
+      path: '/staff/lab-reservation/admin-reservations',
+      title: 'View Reservation Requests',
+      icon: 'mdi:table-cog'
     }
   ]
 }
