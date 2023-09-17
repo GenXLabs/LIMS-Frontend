9ba0091897708888bd3afaf72c8111d7e5c5ad3f
it21256264 <it21256264@my.sliit.lk>
2023-09-17 14:22:16 +0530

    Update index.js

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -141,7 +141,7 @@ const LoginPage = () => {
             margin: theme => theme.spacing(8, 0, 8, 8)
           }}
         >
-          {/* <LoginIllustration alt='login-illustration' src={`/images/pages/${imageSource}-${theme.palette.mode}.png`} /> */}
+          {/* <LoginIllustration alt='login-illustration' src={`/images/pages/login.jpg`} /> */}
           <FooterIllustrationsV2 />
         </Box>
       ) : null}
