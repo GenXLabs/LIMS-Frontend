0bc5962a124aab8c5c706ce0c1e8f332eb284d71
Dhanuka001 <137430860+Dhanuka001@users.noreply.github.com>
2023-09-19 16:12:53 +0530

    timetable

--- src/pages/student/practical-timetable/index.js
+++ src/pages/student/practical-timetable/index.js
@@ -1,6 +1,109 @@
-import { Typography } from '@mui/material'
+// ** React Imports
+import { useEffect, useState } from 'react'
 
-const PracticalTimetable = () => {
-  return <Typography>timetable</Typography>
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+// ** Redux Imports
+import { useDispatch, useSelector } from 'react-redux'
+
+// ** Hooks
+import { useSettings } from 'src/@core/hooks/useSettings'
+
+// ** FullCalendar & App Components Imports
+import Calendar from 'src/views/apps/calendar/Calendar'
+import CalendarWrapper from 'src/@core/styles/libs/fullcalendar'
+import AddEventSidebar from 'src/views/apps/calendar/AddEventSidebar'
+
+// ** Actions
+import {
+  addEvent,
+  fetchEvents,
+  deleteEvent,
+  updateEvent,
+  handleSelectEvent,
+  handleAllCalendars,
+  handleCalendarsUpdate
+} from 'src/store/apps/calendar'
+
+// ** CalendarColors
+const calendarsColor = {
+  'LAB-01': 'info',
+  'LAB-02': 'warning'
 }
-export default PracticalTimetable
+
+const AppCalendar = () => {
+  // ** States
+  const [calendarApi, setCalendarApi] = useState(null)
+  const [leftSidebarOpen, setLeftSidebarOpen] = useState(false)
+  const [addEventSidebarOpen, setAddEventSidebarOpen] = useState(false)
+
+  const [values, setValues] = useState({})
+
+  // ** Hooks
+  const { settings } = useSettings()
+  const dispatch = useDispatch()
+  const store = useSelector(state => state.calendar)
+
+  // ** Vars
+  const leftSidebarWidth = 300
+  const addEventSidebarWidth = 400
+  const { skin, direction } = settings
+  const mdAbove = useMediaQuery(theme => theme.breakpoints.up('md'))
+  useEffect(() => {
+    dispatch(fetchEvents(store.selectedCalendars))
+  }, [dispatch, store.selectedCalendars])
+  const handleLeftSidebarToggle = () => setLeftSidebarOpen(!leftSidebarOpen)
+  const handleAddEventSidebarToggle = () => setAddEventSidebarOpen(!addEventSidebarOpen)
+
+  return (
+    <CalendarWrapper
+      className='app-calendar'
+      sx={{
+        boxShadow: skin === 'bordered' ? 0 : 6,
+        ...(skin === 'bordered' && { border: theme => `1px solid ${theme.palette.divider}` })
+      }}
+    >
+      <Box
+        sx={{
+          p: 6,
+          pb: 0,
+          flexGrow: 1,
+          borderRadius: 1,
+          boxShadow: 'none',
+          backgroundColor: 'background.paper',
+          ...(mdAbove ? { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 } : {})
+        }}
+      >
+        <Calendar
+          store={store}
+          dispatch={dispatch}
+          direction={direction}
+          updateEvent={updateEvent}
+          calendarApi={calendarApi}
+          calendarsColor={calendarsColor}
+          setCalendarApi={setCalendarApi}
+          handleSelectEvent={handleSelectEvent}
+          handleLeftSidebarToggle={handleLeftSidebarToggle}
+          handleAddEventSidebarToggle={handleAddEventSidebarToggle}
+          setValues={setValues}
+        />
+      </Box>
+      <AddEventSidebar
+        store={store}
+        dispatch={dispatch}
+        addEvent={addEvent}
+        updateEvent={updateEvent}
+        deleteEvent={deleteEvent}
+        calendarApi={calendarApi}
+        drawerWidth={addEventSidebarWidth}
+        handleSelectEvent={handleSelectEvent}
+        addEventSidebarOpen={addEventSidebarOpen}
+        handleAddEventSidebarToggle={handleAddEventSidebarToggle}
+      />
+    </CalendarWrapper>
+  )
+}
+
+export default AppCalendar
