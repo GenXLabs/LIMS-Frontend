b8d92f68a403a5b5ff82429cf908e6398e9cc27a
Sehana01 <139713372+Sehana01@users.noreply.github.com>
2023-09-19 14:27:57 +0530

    updated practical manual index file

--- src/pages/staff/practical-manual/index.js
+++ src/pages/staff/practical-manual/index.js
@@ -1,50 +1,62 @@
-// import React from 'react'
-import { styled } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import Divider from '@mui/material/Divider'
-import { TextField } from '@mui/material'
-import CustomTextField from 'src/@core/components/mui/text-field'
-import Icon from 'src/@core/components/icon'
-import ModuleTable from 'src/views/table/ModuleTable'
+import React from 'react'
 
-const Container = styled(Box)(({ theme }) => ({
-  width: '100%',
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column'
-}))
+// ** MUI Imports
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableRow from '@mui/material/TableRow'
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
 
-const UserAdd = styled(Box)(({ theme }) => ({
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  marginTop: '5px',
-  marginBottom: '20px',
-  padding: '10px',
-  borderRadius: '5px',
-  boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.15)'
-}))
+const createData = (name, calories, fat, carbs, protein) => {
+  return { name, calories, fat, carbs, protein }
+}
 
-const ModuleCategory = () => {
+const rows = [
+  createData('Module 1', 159, 6.0, 24, 4.0),
+  createData('Module 2', 237, 9.0, 37, 4.3),
+  createData('Module 3', 262, 16.0, 24, 6.0),
+  createData('Module 4', 305, 3.7, 67, 4.3),
+  createData('Module 5', 356, 16.0, 49, 3.9)
+]
+
+function usersTable() {
   return (
-    <Container>
-      <UserAdd>
-        <Typography variant='h6'>show</Typography>
-        <Box>
-          <CustomTextField sx={{ mr: 4 }} placeholder='Search Module' />
-          <Button variant='contained' sx={{ '& svg': { mr: 2 } }}>
-            <Icon fontSize='1.125rem' icon='tabler:plus' />
-            Add Module
-          </Button>
-        </Box>
-      </UserAdd>
-      <Divider />
-    </Container>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>Username</TableCell>
+            <TableCell align='right'>first name</TableCell>
+            <TableCell align='right'>Last name</TableCell>
+            <TableCell align='right'>Email</TableCell>
+            <TableCell align='right'></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow
+              key={row.name}
+              sx={{
+                '&:last-of-type td, &:last-of-type th': {
+                  border: 0
+                }
+              }}
+            >
+              <TableCell component='th' scope='row'>
+                {row.name}
+              </TableCell>
+              <TableCell align='right'>{row.calories}</TableCell>
+              <TableCell align='right'>{row.fat}</TableCell>
+              <TableCell align='right'>{row.carbs}</TableCell>
+              <TableCell align='right'>{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
-export default ModuleCategory
+export default usersTable
