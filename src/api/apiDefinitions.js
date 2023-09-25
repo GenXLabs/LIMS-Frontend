9541891bd979d16da8b982e32bba48542d547fbd
Sandupa Abeywardana <74253035+SandupaAbeywardana@users.noreply.github.com>
2023-09-25 14:45:57 +0530

    update

--- src/api/apiDefinitions.js
+++ src/api/apiDefinitions.js
@@ -14,7 +14,11 @@ export default {
 
   updateReservationStatus: async function (reservationID, status) {
     return await api.put(`/lab-reservation/update-status/${reservationID}/${status}`)
-  }
+  },
 
   /* Lab Reservation APIs End */
+
+  sendEmail: async function (emailPayload) {
+    return await api.post(`/email/send-email`, emailPayload)
+  }
 }
