72da56c559bae3910a34387bfb7287946b0e8dc6
Sandupa Abeywardana <74253035+SandupaAbeywardana@users.noreply.github.com>
2023-10-14 11:04:57 +0530

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,26 +7,28 @@ import CardContent from '@mui/material/CardContent'
 
 const Home = () => {
   return (
-    <Grid
-      container
-      spacing={6}
-      sx={{ display: 'flex', minHeight: '110% !important', justifyContent: 'center', alignItems: 'center' }}
-    >
-      <img height='450' alt='page-under-construction' src='/images/pages/DashUnderProgress.png' />
+    <>
       {/* <Grid
-        item
-        xs={12}
-        sx={{ display: 'flex', minHeight: '100% !important', justifyContent: 'center', alignItems: 'center' }}
+        container
+        spacing={6}
+        sx={{ display: 'flex', minHeight: '110% !important', justifyContent: 'center', alignItems: 'center' }}
       >
-        <Card>
-          <CardHeader title='Student Dashboard'></CardHeader>
-          <CardContent>
-            <img height='400' alt='page-under-construction' src='/images/pages/DashUnderProgress.png' />
-          </CardContent>
-        </Card>
-        <img height='400' alt='page-under-construction' src='/images/pages/DashUnderProgress.png' />
+        <img height='450' alt='page-under-construction' src='/images/pages/DashUnderProgress.png' />
+        <Grid
+          item
+          xs={12}
+          sx={{ display: 'flex', minHeight: '100% !important', justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Card>
+            <CardHeader title='Student Dashboard'></CardHeader>
+            <CardContent>
+              <img height='400' alt='page-under-construction' src='/images/pages/DashUnderProgress.png' />
+            </CardContent>
+          </Card>
+          <img height='400' alt='page-under-construction' src='/images/pages/DashUnderProgress.png' />
+        </Grid>
       </Grid> */}
-    </Grid>
+    </>
   )
 }
 
