953da8c6f274c52c43e26a2fb46759927347a0e8
Sandupa Abeywardana <74253035+SandupaAbeywardana@users.noreply.github.com>
2023-10-14 14:41:30 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 /* Local Backend */
-const BaseURL = 'http://localhost:8082/api/v1/lims'
+const BaseURL = 'http://13.251.138.245:8082/api/v1/lims'
 
 export const api = axios.create({
   baseURL: BaseURL
